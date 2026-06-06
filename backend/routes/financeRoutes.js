defe44e9c1c4a0eb39703cc7812b71245d46dc0a
rishithareddy10987 <rishitha.reddy3008@gmail.com>
2026-06-06 18:41:56 +0530

    My current work backup

--- backend/routes/financeRoutes.js
+++ backend/routes/financeRoutes.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getGoals, createGoal, updateGoal, deleteGoal } = require('../controllers/goalController');
+const { getExpenses, addExpense } = require('../controllers/financeController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.route('/')
-  .get(protect, getGoals)
-  .post(protect, createGoal);
-
-router.route('/:id')
-  .put(protect, updateGoal)
-  .delete(protect, deleteGoal);
+router.route('/').get(protect, getExpenses).post(protect, addExpense);
 
 module.exports = router;
